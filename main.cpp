e76db2f4a5d839daeda4217dfc9733a0a39e31d5
Alexandre Delétraz <alexandre.deletraz@hotmail.com>
2022-11-15 10:46:14 +0100

    premier commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*
+  ---------------------------------------------------------------------------
+  Fichier     : <fichier>.cpp
+  Nom du labo : à compléter
+  Auteur(s)   : Delétraz Alexandre
+  Date        : à compléter
+  But         : le but du programme et non le but du laboratoire !!
+
+  Remarque(s) : à compléter
+
+  Compilateur : gcc version 11.2.0
+  ---------------------------------------------------------------------------
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include <limits>
+
+#define VIDER_BUFFER cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+using namespace std;
+
+int main(){
+
+
+
+   VIDER_BUFFER;
+   return EXIT_SUCCESS;
+}
